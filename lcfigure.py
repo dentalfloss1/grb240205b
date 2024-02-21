a2e987198f6928f71bb0e5aae6f1435962ed8398
Sarah Chastain <schastain@gwu.edu>
2024-02-21 11:50:23 -0700

    begin lc figure

---
+++ lcfigure.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+import datetime
+import numpy as np
+
+trigger = datetime.datetime(2024, 2, 5, 22, 15, 8, 00)
+
+plotdata = pd.read_csv("grbmeas.csv")
+startdate = [(datetime.datetime.strptime(d, "%Y-%m-%d %H:%M:%S.%f") - trigger).total_seconds()/3600/24 for d in plotdata['start']]
+stopdate = [(datetime.datetime.strptime(d, "%Y-%m-%d %H:%M:%S.%f") - trigger).total_seconds()/3600/24 for d in plotdata['stop']]
+obsdur = [(t2-t1) for t1,t2 in zip(startdate,stopdate)]
+plotdata['obsdate'] = [t1+(dur/2) for t1,dur in zip(startdate,obsdur)]
+
+fig = plt.figure()
+
+for freq in np.sort(np.unique(plotdata['freq'])):
+    curdata = plotdata[plotdata['freq']==freq]
+    xdata = curdata['obsdate']
+    ydata = curdata['flux']*1e-6
+    yerr = curdata['err']
+    plt.plot(xdata,ydata,label=f'{freq} GHz')
+ax = plt.gca()
+ax.set_xscale('log')
+ax.set_yscale('log')
+plt.legend()
+plt.savefig("lightcurve.png")
+plt.close()
+    
