af198a895a5aac717d6d859c937b25e73fde0d7c
Sarah Chastain <sarahchastain1@unm.edu>
2024-07-25 15:52:43 -0600

    updated measurements

--- lcfigure.py
+++ lcfigure.py
@@ -10,13 +10,14 @@ startdate = [(datetime.datetime.strptime(d, "%Y-%m-%d %H:%M:%S.%f") - trigger).t
 stopdate = [(datetime.datetime.strptime(d, "%Y-%m-%d %H:%M:%S.%f") - trigger).total_seconds()/3600/24 for d in plotdata['stop']]
 obsdur = [(t2-t1) for t1,t2 in zip(startdate,stopdate)]
 plotdata['obsdate'] = [t1+(dur/2) for t1,dur in zip(startdate,obsdur)]
-plotdata = plotdata[np.isin(plotdata['freq'],[5.5,9.0])]
-fig,axs = plt.subplots(2,1,figsize=(10,15),sharex=True,sharey=True)
+plotdata['startdate'] = startdate
+plotdata['stopdate'] = stopdate
+# plotdata = plotdata[np.isin(plotdata['freq'],[5.5,9.0])]
+fig,axs = plt.subplots(5,1,figsize=(7,15),sharex=True,sharey=True)
 
 for freq,ax in zip(np.sort(np.unique(plotdata['freq'])),axs):
     curdata = plotdata[plotdata['freq']==freq]
     xdata = curdata['obsdate']
-    print(xdata)
     ydata = curdata['flux']*1e-6
     yerr = np.sqrt(curdata['err']**2 + curdata['rms']**2)*1e-6
     ax.scatter(xdata,ydata,label=f'{freq} GHz')
@@ -24,12 +25,41 @@ for freq,ax in zip(np.sort(np.unique(plotdata['freq'])),axs):
     ax.set_title(f'{freq} GHz')
     ax.set_xscale('log')
     ax.set_yscale('log')
-    if freq==17.0:
-        ax.set_xlabel("Days post trigger")
     ax.set_ylabel("Flux Density (Jy)")
     ax.set_ylim(1e-5,3e-3)
+    for o in np.sort(curdata['obs']):
+        subdata = curdata[curdata['obs']==o]
+        print(subdata)
+        startobs = subdata['startdate'].min()
+        endobs = subdata['stopdate'].max()
+        ax.axvspan(startobs,endobs, alpha=0.15, color='gray')
 # plt.legend()
+ax.set_xlabel("Days post-trigger")
 plt.tight_layout()
 plt.savefig("lightcurve.png")
 plt.close()
     
+fig = plt.figure()
+freq = 9
+curdata = plotdata[plotdata['freq']==9]
+xdata = curdata['obsdate']
+ydata = curdata['flux']*1e-6
+yerr = np.sqrt(curdata['err']**2 + curdata['rms']**2)*1e-6
+plt.scatter(xdata,ydata,label=f'{freq} GHz')
+plt.errorbar(xdata,ydata,yerr=yerr,fmt=' ')
+ax = plt.gca()
+ax.set_title(f'{freq} GHz')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_ylabel("Flux Density (Jy)")
+ax.set_ylim(1e-5,3e-3)
+for o in np.sort(curdata['obs']):
+    subdata = curdata[curdata['obs']==o]
+    print(subdata)
+    startobs = subdata['startdate'].min()
+    endobs = subdata['stopdate'].max()
+    plt.axvspan(startobs,endobs, alpha=0.15, color='gray')
+ax.set_xlabel("Days post-trigger")
+plt.tight_layout()
+plt.savefig("9GHzlc.png")
+plt.close()
