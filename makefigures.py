might have fixed the relative normalization

makefigures.py
@@ -147,14 +147,15 @@ else:
     def theory_bigsbpl(ivar, f0, nu0_1, nu0_2, k):
         t0 = 1
         d=0.4
-        s = 10
+        s = 5
         a1 = -k/(2*(4-k))
         b1 = -3*k/(5*(4-k))
         b2 = -3/2
         p = 2.2
         nu0_3 = 1e9
         t, nu = ivar
-        res = []
+        y1 = []
+        y2 = []
         t_break = np.amax(t)
         for tval in np.sort(t):
             nua = nu0_1*(tval/t0)**b1
@@ -178,6 +179,7 @@ else:
                 nubreak1_1 = nua_1
                 nubreak2_1 = num_1
             else:
+                fnu_m1 = f0*(tval/t0)**a1
                 c1_1 = 1/3
                 c2_1 = -(p-1)/2
                 c3_1 = -1
@@ -195,23 +197,32 @@ else:
             num_break2 = nu0_1*(t_break/t0)**b1_2
             nua_break2 = nu0_2*(t_break/t0)**b2_2
             fnu_m_2 = f0*(tval/t0)**a1_2
-            fpk = f0*(num_2/nua_2)**(3)
+            fpk = fnu_m_2*(num_2/nua_2)**(3)
             fpk_2 = fpk
-            if tval < t_break:
-                result = dsbpl(nuval,fpk_1,nubreak1_1,c1_1,c2_1,nubreak2_1,c3_1,s)
-            else:
-               #  break1val = dsbpl(nuval,f0*(t_break/t0)**a1,nubreak1_1,c1_1,c2_1,nubreak2_1,c3_1,s)
-               #  break2val = dsbpl(nuval,f0*(t_break/t0)**a1_2,num_break2,c1_2,c2_2,nua_break2,c3_2,s)
-               #  result = break1val*dsbpl(nuval,fpk_2,num_2,c1_2,c2_2,nua_2,c3_2,s)/break2val
-               result = dsbpl(nuval,fpk_2,num_2,c1_2,c2_2,nua_2,c3_2,s)
+            res1 = dsbpl(nuval,fpk_1,nubreak1_1,c1_1,c2_1,nubreak2_1,c3_1,s)
+           
+            c1_1 = 2
+            c2_1 = 1/3
+            c3_1 = -(p-1)/2
+            num_1 = nu0_1*(t_break/t0)**b1_1
+            nua_1 = nu0_2*(t_break/t0)**(b2_1)
+            fnu_m1 = f0*(t_break/t0)**a1
+            fpk_1 = fnu_m1*(nua_1/num_1)**(1/3)
+            F_bk1 = dsbpl(nuval,fpk_1,num_1,c1_1,c2_1,nua_1,c3_1,s)
+            fpk2_break = f0*(t_break/t0)**a1_2*(num_break2/nua_break2)**(3)
+            F_bk2 = dsbpl(nuval,fpk2_break,num_break2,c1_2,c2_2,nua_break2,c3_2,s)
+            res2 = F_bk1*dsbpl(nuval,fpk_2,num_2,c1_2,c2_2,nua_2,c3_2,s)/F_bk2
+            y1.append(res1)
+            y2.append(res2)
 
-            res.append(result)
-        return np.array(res)
-    initial_guess = [4e-4,15,50,2]
-    bounds = [(1e-6,1),(1,100),(1,300),(0,3)]
+        result = np.where( t<=t_break,np.array(y1),np.array(y2))
+        return result
+
+    initial_guess = [1e-3,9,20,2]
+    bounds = [(1e-6,5e-3),(1,100),(1,100),(0,2.1)]
     bounds0 = tuple([b[0] for b in bounds])
     bounds1 = tuple([b[1] for b in bounds])
-    curdata = plotdata[(plotdata['obsdate'] > 1) ]
+    curdata = plotdata[ (plotdata['obsdate'] >10)]
     bounds = [bounds0,bounds1]
     tdata = curdata['obsdate']
     nudata = curdata['freq']
@@ -227,7 +238,7 @@ else:
         vnum = ind+1
         text = f" {var}={popt[vnum]}+/-{np.absolute(pcov[vnum][vnum])**0.5}"
         print(text)
-    print("d=0.4")
+    print("s=10")
 #     k = 2
 #     p = 2
 #     a1 = -k/(2*(4-k))
@@ -457,17 +468,17 @@ for band,ax in zip(bands,axs):
     ax.set_ylabel("Flux Density (Jy)")
     ax.set_xlim(1e-2,365)
     ax.set_ylim(1e-5,3e-3)
-    test_theory = [1.e-3, 9 , 50 , 2]
+    test_theory = [1e-3, 18.5, 60.3 , 2]
     t_break = get_tbreak((xline,nu),*test_theory)
-    print(test_theory)
+    # print(test_theory)
     ax.axvline(t_break,ls='-')
     yline = theory_bigsbpl((xline,nu), *test_theory)
     ax.plot(xline,yline,color='black',alpha=0.5,ls=':',label='theory')
     yline = verify_powerlaw(xline,1e-3,2,0.5,0.5,2,-0.75)
-    ax.plot(xline,yline,color='black',alpha=0.5,ls='-')
+    # ax.plot(xline,yline,color='black',alpha=0.5,ls='-')
 
 
-#     test_theory = [1.e-3, 9 , 50 , 0]
+#     test_theory = [1.e-3, 58 , 95 , 0]
 #     print(test_theory)
 #     t_break = get_tbreak((xline,nu),*test_theory)
 #     ax.axvline(t_break,ls=':')